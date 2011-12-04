73cdb22f3aed1285252c33daa80107fed99d6d46
benoertel <benjamin.oertel@me.com>
2011-12-05 00:07:30 +0100

    Auth_Login form dummy.

---
+++ application/forms/auth/Login.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Application_Form_Auth_Login extends Zend_Form
+{
+    
+}
+?>
