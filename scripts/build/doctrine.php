5a80cdc2795a139404c681e9a3c3348a27d0ddd8
Dominik Horb <dominik.horb@googlemail.com>
2011-12-17 23:49:10 +0100

    Fixed error in application path

--- scripts/build/doctrine.php
+++ scripts/build/doctrine.php
@@ -1,7 +1,7 @@
 <?php
 
 define('APPLICATION_ENV', 'development');
-define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../../application'));
  echo APPLICATION_PATH;
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
