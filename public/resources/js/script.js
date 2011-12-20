aff5fcd191fefdf2758cb3b40db456131efcb2c4
Dominik Horb <dominik.horb@googlemail.com>
2011-12-20 17:56:19 +0100

    Added just a simple js idea on hiding the upper toolbar

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,6 +1,16 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function(){
+  
+  var dropdownButton = $('<span class="dropdown-button arrow-up"></span>');
+  dropdownButton.toggle(function(){
+    $('#main-header').css('top', '-30px').css('margin-bottom', '5px');
+    dropdownButton.removeClass('arrow-up');
+    dropdownButton.addClass('arrow-down');
+  }, function(){
+    $('#main-header').css('top', '0').css('margin-bottom', '35px');
+    dropdownButton.addClass('arrow-up');
+    dropdownButton.removeClass('arrow-down');
+  });
+  $('#main-header').append(dropdownButton);
+});
 
 
