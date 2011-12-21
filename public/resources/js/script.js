b329b0915c1532d09a55f05a5a6223e998b3503a
Dominik Horb <dominik.horb@googlemail.com>
2011-12-21 23:45:02 +0100

    Changes to css, addition of logo

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
   var dropdownButton = $('<span class="dropdown-button arrow-up"></span>');
   dropdownButton.toggle(function(){
     //$('#main-header').slideUp();
-    $('#main-header').css('top', '-32px').css('margin-bottom', '5px');
+    $('#main-header').css('top', '-32px').css('margin-bottom', '80px');
     dropdownButton.removeClass('arrow-up');
     dropdownButton.addClass('arrow-down');
   }, function(){
-    $('#main-header').css('top', '0').css('margin-bottom', '35px');
+    $('#main-header').css('top', '0').css('margin-bottom', '115px');
     dropdownButton.addClass('arrow-up');
     dropdownButton.removeClass('arrow-down');
   });
-  $('#main-header').append(dropdownButton);
+  $('#settings-panel').append(dropdownButton);
 });
 
 
