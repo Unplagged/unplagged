98df1868e7646ac755154c307df19b704c9b272a
Dominik Horb <dominik.horb@googlemail.com>
2012-01-11 15:26:33 +0100

    Added new mime type mappings for parsers

--- library/Unplagged/Parser.php
+++ library/Unplagged/Parser.php
@@ -13,12 +13,14 @@
 class Unplagged_Parser{
 
   private static $mimeMappings = array(
-    'image/tif'=>'TesseractParser'
-    , 'image/tiff'=>'ImageParser'
+    'image/tif' => 'TesseractParser'
+    , 'image/tiff' => 'ImageParser'
     //@todo sprobably not the best idea to push all octet-stream through tesseract, but will work for now
-    , 'application/octet-stream'=>'TesseractParser'
-    , 'image/jpeg'=>'ImageParser'
-    , 'image/gif'=>'ImageParser'
+    , 'application/octet-stream' => 'TesseractParser'
+    , 'image/jpeg' => 'ImageParser'
+    , 'image/gif' => 'ImageParser'
+    , 'image/png' => 'ImageParser'
+    , 'image/jpg' => 'ImageParser'
   );
 
   public static function factory($mimeType){
