1d5526720fe3634827f0ee3fb350121929dd45ab
elsa <Gast@.(none)>
2012-01-12 19:24:06 +0100

    search

---
+++ application/controllers/GooglesearchController2.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class GoogleSearchController extends Zend_Controller_Action
+{
+    
+    public function indexAction()
+    {
+        // action body
+        $searchForm = new Application_Form_Google_Search;
+       /* $searchForm->setAction('http://www.google.de/search');
+        $searchForm->setMethod('GET');*/
+
+        $this->view->searchForm = $searchForm;
+    }
+    
+    public function searchAction()
+    {
+        
+    }
+    
+ 
+ }
+?>
