363cf2b875ffa36ea40cc12ca40635be33c4a9ba
Benjamin Oertel <benjamin.oertel@me.com>
2012-04-13 10:46:33 +0200

    adds missing page position model

---
+++ application/models/Document/Page/Position.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Unplagged - The plagiarism detection cockpit.
+ * Copyright (C) 2012 Unplagged
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * The class represents a fragment within a document.
+ * It defines also the structure of the database table for the ORM.
+ *
+ * @author Benjamin Oertel <mail@benjaminoertel.com>
+ * @version 1.0
+ * 
+ * @Entity 
+ * @Table(name="document_page_positions")
+ * @HasLifeCycleCallbacks
+ */
+class Application_Model_Document_Page_Position extends Application_Model_Base{
+
+  /**
+   *
+   * @ManyToOne(targetEntity="Application_Model_Document_Page")
+   * @JoinColumn(name="page_id", referencedColumnName="id")
+   */
+  private $page;
+
+  /**
+   * The line position.
+   * 
+   * @Column(type="integer", length=64)
+   */
+  private $linePos;
+  
+  /**
+   * The character position.
+   * 
+   * @Column(type="integer", length=64)
+   */
+  private $characterPos;
+
+  
+  public function __construct(array $data = null){
+    
+  }
+
+  public function getDirectName(){
+    return "document_page_position";
+  }
+
+  public function getDirectLink(){
+    return "/document-page-position/show/id/" . $this->id;
+  }
+
+  public function getIconClass(){
+    return "document-icon";
+  }
+  
+  public function getPage(){
+    return $this->page;
+  }
+
+  public function getLinePos(){
+    return $this->linePos;
+  }
+
+  public function getCharacterPos(){
+    return $this->characterPos;
+  }
+
+
+
+}
