80aeb4f1ae1dab5cd93220d0e323ab935458e5be
Benjamin Oertel <benjamin.oertel@me.com>
2012-04-24 00:35:07 +0200

    fixes pdf generation bug on staging

--- scripts/jobs/init.php
+++ scripts/jobs/init.php
@@ -12,7 +12,7 @@ defined('APPLICATION_PATH')
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'staging'));
 
 /**
  * @const TEMP_PATH The path to the directory where temporary data should be stored.
