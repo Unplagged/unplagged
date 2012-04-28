f4ab38e330d901465a4f0abbb0b048f43704fee6
Benjamin Oertel <benjamin.oertel@me.com>
2012-04-28 23:06:37 +0200

    updates document state after parsing correctly

--- scripts/jobs/Document/Parser.php
+++ scripts/jobs/Document/Parser.php
@@ -54,7 +54,7 @@ class Cron_Document_Parser extends Cron_Base{
       
       if($document instanceof Application_Model_Document){
         // update document
-        $document->setState(self::$em->getRepository('Application_Model_State')->findOneByName("document_parsed"));
+        $document->setState(self::$em->getRepository('Application_Model_State')->findOneByName("parsed"));
 
         // update task
         $task->setState(self::$em->getRepository('Application_Model_State')->findOneByName("task_finished"));
