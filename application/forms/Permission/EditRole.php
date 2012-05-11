c529f570d77c029697921bcaab0370cbda5e37c7
Dominik Horb <dominik.horb@googlemail.com>
2012-05-11 15:25:50 +0200

    Some more acl

---
+++ application/forms/Permission/EditRole.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Unplagged - The plagiarism detection cockpit.
+ * Copyright (C) 2012 Unplagged
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * 
+ * @author Unplagged
+ */
+class Application_Form_Permission_EditRole extends Zend_Form{
+
+  private $permissions;
+
+  public function init(){
+    $this->setMethod('post');
+
+
+    foreach($this->permissions as $groupLabel=>$permissionGroup){
+      $elements = $permissionGroup;
+      foreach($permissionGroup as $permission){
+        $checkboxElement = new Zend_Form_Element_Checkbox($permission, array('belongsTo'=>$groupLabel));
+        $checkboxElement->setLabel($permission);
+        $checkboxElement->setOptions(array('class'=>'btn btn-primary  inherited btn-checkbox'));
+        $this->addElement($checkboxElement);
+      }
+     $this->addDisplayGroup($elements, $groupLabel, array('legend'=>$groupLabel));
+    }
+    $submitElement = new Zend_Form_Element_Submit('submit');
+    $submitElement->setLabel('Save');
+    $submitElement->setOptions(array('class'=>'btn btn-primary'));
+
+
+    $this->addElements(array($submitElement));
+  }
+
+  public function setPermissions($permissions){
+    $this->permissions = $permissions;
+  }
+
+}
+?>
