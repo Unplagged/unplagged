2e2589d56abf0e7b027b0f04ca1e588527cbd810
Benjamin Oertel <benjamin.oertel@me.com>
2012-06-14 20:10:38 +0200

    some small changes

--- scripts/jobs/init.php
+++ scripts/jobs/init.php
@@ -12,31 +12,44 @@ defined('BASE_PATH')
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../../application'));
 
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (isset($arguments["e"]) ? $arguments["e"] : 'development'));
-
 /**
  * @const TEMP_PATH The path to the directory where temporary data should be stored.
  */
 defined('TEMP_PATH')
     || define('TEMP_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'temp');
 
-// Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
+/**
+ * @const APPLICATION_ENV The application environment, from which the config values are taken. Defaults to the most 
+ * secure environment 'production', but only if nothing has been set before, e. g. by defininig it in the vhost.conf or 
+ * some similar meachanism.
+ */
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (isset($arguments["e"]) ? $arguments["e"] : 'production'));
+
+require_once BASE_PATH . '/scripts/build/Installer/Installer.php';
+
+$installer = new Installer();
+if($installer->isInstalled()){
+  // Ensure library folder is on the include_path
+  set_include_path(implode(PATH_SEPARATOR, array(
       realpath(APPLICATION_PATH . '/../library'),
       get_include_path(),
-    )));
-
-/** Zend_Application */
-require_once 'Zend/Application.php';
-
-// Create application, bootstrap, and run
-$application = new Zend_Application(
-        APPLICATION_ENV,
-        APPLICATION_PATH . '/configs/application.ini'
-);
-$application->bootstrap();
+  )));
+  
+  require_once 'Zend/Application.php';
+  
+  //Create application, bootstrap, and run
+  $application = new Zend_Application(APPLICATION_ENV, array(
+    'config'=>array(
+      APPLICATION_PATH . '/configs/application.ini',
+      APPLICATION_PATH . '/configs/unplagged-config.ini'
+    )
+  ));
+  $application->bootstrap();
+  $application->run();
+} else {
+  $installer->install();
+}
 
 register_shutdown_function('__shutdown');
 
